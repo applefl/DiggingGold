c6afd0b93bbf72361ade67854dadaa0d9e47b6f2
applefl <719235168@qq.com>
2017-11-22 19:25:22 +0800

    微信服务器

---
+++ config.js
@@ -0,0 +1,15 @@
+/*
+* @Author: Marte
+* @Date:   2017-11-22 18:49:44
+* @Last Modified by:   Marte
+* @Last Modified time: 2017-11-22 18:50:31
+*/
+
+'use strict';
+
+var config = {
+    token: 'xxxxxxx',
+    appid: 'xxxxxxxxxxx',
+    encodingAESKey: 'xxxxxxxxxxxxxxxxxxxxxxxx'
+};
+module.exports = config;
